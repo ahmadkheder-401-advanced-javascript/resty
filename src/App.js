2067c9c8e86dfd502f0dfecc0599ef55745e7d96
ahmadkheder <ahmadkhederabdulatef@gmail.com>
2020-10-13 00:32:09 +0300

    props-and-state-work

--- src/App.js
+++ src/App.js
@@ -3,37 +3,31 @@ import React from 'react';
 import Main from './components/main/main';
 import Header from './components/header/header'
 import Footer from './components/footer/footer'
+import Results from './components/Results/Results'
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        url: "",
-        method: "",
-        hits:[]
-    };
+      count:0,
+      result:[],
+      headers:[]
+      };
+    }
     
-}
+    urlHandler = (count,result) =>{
+      this.setState({count,result});
+    }
 render(){
 
   return (
     <div className="App">
-      {/* <header className="App-header"> */}
         <Header />
-        <Main/>
-          {/* {
-            <div>
-              <h1>hi</h1>
-              <p>{Main.state}</p>
-            </div>
-          } */}
-        {/* </Main> */}
-        {/* <Main/>
-        {Main.state} */}
-        {/* <Main/> */}
+        <Main formData ={this.urlHandler}/>
+        {console.log('urlData', this.state.url)}
+        <Results count={this.state.count} headers={this.state.headers} results={this.state.result} />
         <Footer />
 
 
-      {/* </header> */}
     </div>
   );
 }
