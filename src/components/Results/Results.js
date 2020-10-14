4dbaf59088b8cf834f424e960c6f5b2828fbb01c
ahmadkheder <ahmadkhederabdulatef@gmail.com>
2020-10-14 03:53:35 +0300

    incomplete

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactJson from 'react-json-view'
-
+import { If, Then, Else } from '../if/If';
+import JSONPretty from 'react-json-pretty';
 class Results extends React.Component {
 
     constructor(props) {
@@ -11,9 +12,27 @@ class Results extends React.Component {
         
         console.log('proops', this.props);
         return (
-            <>
-            <ReactJson src={this.props} theme="monokai" />
-          </>
+            <If condition={props}>
+      <div>
+        <section className='container'>
+          <h3> Count: {props.Count}</h3>
+          <h3> Headers:  </h3>
+          <p> <JSONPretty data ={props.headers} /></p>
+          <h3> Results: </h3>
+          <ul>
+            {props.results.map(result => {
+              return(
+                <li key={result.name}> <JSONPretty data= {result}/></li>
+              );
+            })}
+          </ul>
+           
+        </section>
+      </div>
+    </If>
+        //     <>
+        //     <ReactJson src={this.props} theme="monokai" />
+        //   </>
         )
     }
 }
---
+++ src/components/history/history.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class History extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state= {
+     
+    };
+
+  }
+
+  render() {
+    
+    return (
+      <></>
+    );
+  }
+}
+
+  
+export default History;
