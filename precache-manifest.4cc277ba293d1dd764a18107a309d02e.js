f81623ec2f38bbff2c7e3e106bd084be02d8df73
AhmadKheder <runner@fv-az21-141.uic1inuc2plufdycpaqvb1bjtg.bx.internal.cloudapp.net>
2020-11-05 00:07:03 +0000

    Updates

---
+++ precache-manifest.4cc277ba293d1dd764a18107a309d02e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce31ae75c534c8c674e477cc71ee2281",
+    "url": "/resty/index.html"
+  },
+  {
+    "revision": "d57db5e03a87b5e35a5c",
+    "url": "/resty/static/css/main.b01ebad3.chunk.css"
+  },
+  {
+    "revision": "be171385171324177414",
+    "url": "/resty/static/js/2.e3b616c7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/resty/static/js/2.e3b616c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d57db5e03a87b5e35a5c",
+    "url": "/resty/static/js/main.6d832102.chunk.js"
+  },
+  {
+    "revision": "a82df10f567bbeaa6ec2",
+    "url": "/resty/static/js/runtime-main.ea9bec24.js"
+  }
+]);
